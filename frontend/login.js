2291544619e3f779d3dde3a4c6b821880ef8dc23
DERICK-MUNENE <147225426+DERICK-MUNENE@users.noreply.github.com>
2026-03-25 15:13:19 +0300

    Update login.js

--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,4 @@
-const API_URL = "https://microwealthapp-production.up.railway.app/"; 
+const API_URL = "https://microwealthapp-production.up.railway.app"; 
 
 const form = document.getElementById("loginForm");
 const email = document.getElementById("email");
