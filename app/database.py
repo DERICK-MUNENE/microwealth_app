8dfad4ec6e9a825a59b0ad9372da2cd945e941d9
DERICK-MUNENE <147225426+DERICK-MUNENE@users.noreply.github.com>
2026-03-25 10:04:31 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,16 +1,14 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_NAME = os.environ.get("DB_NAME")
+DB_PORT = os.environ.get("DB_PORT", 3306)
 
-DB_USER = os.environ.get("DB_USER", "if0_38916805")         
-DB_PASS = os.environ.get("DB_PASS", "")              
-DB_HOST = os.environ.get("DB_HOST", "sql311.infinityfree.com")     
-DB_NAME = os.environ.get("DB_NAME", "if0_38916805_project")
-
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
-
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
