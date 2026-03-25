f48203900faf641ad9506f3c69caaa5af0913cda
DERICK-MUNENE <147225426+DERICK-MUNENE@users.noreply.github.com>
2026-03-25 12:55:49 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,12 +1,11 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
-DB_HOST = os.environ.get("DB_HOST")
-DB_NAME = os.environ.get("DB_NAME")
-DB_PORT = int(os.environ.get("DB_PORT", 3306))
+DB_USER = "root"
+DB_PASS = "GSsgSqFqtSsQUtmSKDhfEXKrOvpsmtkp"
+DB_HOST = "mysql.railway.internal"
+DB_NAME = "railway"
+DB_PORT = 47635
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
 
