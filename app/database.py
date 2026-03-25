c1216dc12d8b1ad7e20feb5b16726dff02223a5b
DERICK-MUNENE <147225426+DERICK-MUNENE@users.noreply.github.com>
2026-03-25 12:27:21 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 DB_HOST = os.environ.get("DB_HOST")
 DB_NAME = os.environ.get("DB_NAME")
-DB_PORT = os.environ.get("DB_PORT", 47635)
+DB_PORT = os.environ.get("DB_PORT", 3306)
 
 DATABASE_URL = (
     f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
