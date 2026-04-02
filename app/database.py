5953aac61616b52c0c90238f0826b1be489fd481
DERICK-MUNENE <147225426+DERICK-MUNENE@users.noreply.github.com>
2026-04-02 08:39:21 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -9,12 +9,19 @@ DB_PORT = 47635
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,     # checks dead connections
+    pool_recycle=300,       #  refresh before timeout (VERY IMPORTANT)
+    pool_size=5,            #  connection pool size
+    max_overflow=10,        #  extra connections if needed
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Test connection
 try:
-    engine.connect()
-    print("DB connection OK")
+    with engine.connect() as conn:
+        print("DB connection OK")
 except Exception as e:
     print("DB connection failed:", e)
